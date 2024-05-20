f0b27e3b01b28a803ac7b765165c2189dbf85fb4
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-05-20 15:49:06 +0700

    update code

---
+++ Bll/Services/Impliment/BrandServices.cs
@@ -0,0 +1,44 @@
+﻿using Bll.Extentions;
+using Bll.Services.Interface;
+using Data.Entities;
+using Data.Models;
+
+namespace Bll.Services.Impliment
+{
+    public class BrandServices : DbServices, IBrandServices
+    {
+        public BrandServices()
+        {            
+        }
+
+        public bool Create(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll(PagingModels page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataTableResult> List(PagingModels page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bll/Services/Interface/IBrandServices.cs
@@ -0,0 +1,15 @@
+﻿using Data.Entities;
+using Data.Models;
+
+namespace Bll.Services.Interface
+{
+    public interface IBrandServices
+    {
+        public bool Create(Brand brand);
+        public bool Update(Brand brand);
+        public bool Delete(int id);
+        public List<Brand> GetAll(PagingModels page);
+        public Task<DataTableResult> List(PagingModels page);
+        public Brand GetById(int id);
+    }
+}
