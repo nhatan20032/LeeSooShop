cc3557d65c3bcd6a0b6bc88148e35cb5d6a2756a
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-06-14 14:01:32 +0700

    AllowPublicKeyRetrieval in backend service

--- Bll/Extentions/DbServices.cs
+++ Bll/Extentions/DbServices.cs
@@ -4,7 +4,7 @@ namespace Bll.Extentions
 {
     public class DbServices
     {
-        private const string ConnectionString = "Server=localhost;Database=leesooshop;User ID=root;Password=123456;SslMode=none;";
+        private const string ConnectionString = "Server=localhost;Database=leesooshop;User ID=root;Password=123456;SslMode=none;AllowPublicKeyRetrieval=True;";
 
         private static readonly Lazy<OrmLiteConnectionFactory> _lazyConnectionData = new(() =>
         {
